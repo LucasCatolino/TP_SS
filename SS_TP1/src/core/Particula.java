56c349add351146ff2f11541da0950e48b86b251
Lucas <lcatolino@fi.uba.ar>
2022-03-11 20:04:46 -0300

    Particulas inician en posicion random dependiendo de L

--- SS_TP1/src/core/Particula.java
+++ SS_TP1/src/core/Particula.java
@@ -5,8 +5,22 @@ public class Particula {
     private Point pnt;
     private double radius;
 
-    public Particula(Point pnt, double radius) {
-        this.pnt = pnt;
+    public Particula(double radius, double interaction, int L) {
+        this.pnt = randomInit(L);
         this.radius = radius;
     }
+
+	private Point randomInit(int limit) {
+		double x= Math.random()*limit;
+		double y= Math.random()*limit;
+		return new Point(x, y);
+	}
+
+	public double getX() {
+		return pnt.getX();
+	}
+	
+	public double getY() {
+		return pnt.getY();
+	}
 }

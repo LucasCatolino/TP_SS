e0bb801583244d48194d962b730ddc7b507226fe
Lucas <lcatolino@fi.uba.ar>
2022-03-11 12:24:14 -0300

    Agrego inicio de proyecto

---
+++ SS_TP1/src/test/Tests.java
@@ -0,0 +1,22 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+import core.Particula;
+
+public class Tests {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test01CrearParticula() {
+		Particula particula= new Particula();
+		assertNotNull(particula);;
+	}
+
+}
